5fc6e1d705b7fcd3509388e356e5b2977875fe55
PetarPetrov89 <62606018+PetarPetrov89@users.noreply.github.com>
2020-04-02 20:00:47 +0300

    1

---
+++ C-Cours/Danni/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Danni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете потребителско име: ");
+            string username = Console.ReadLine();
+            Console.Write("Въведете електронна поща: ");
+            string mail = Console.ReadLine();
+            Console.Write("Въведете телефонен номер: ");
+            long phoneNumber = long.Parse(Console.ReadLine());
+            Console.Write("Въведете години: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Вие въведохте потребителско име {username}, " +
+                $"неговата електронна поща е {mail}. Потрбителя е на {age} години, " +
+                $"неговият телефонен номер е {phoneNumber}.");
+        }
+    }
+}
---
+++ C-Cours/TwoWords/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text1 = Console.ReadLine();
+            string text2 = Console.ReadLine();
+
+            Console.WriteLine(text1 + text2);
+        }
+    }
+}
